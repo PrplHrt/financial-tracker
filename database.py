5aff62d62fe95ce0fbd05914f046646eaf251cde
Eyad Mohamed Ali <eyad.azim@gmail.com>
2021-07-30 02:26:32 +0400

    idea for classes for database, might be useless

---
+++ database.py
@@ -0,0 +1,33 @@
+class Customer():
+    def __init__(self, n: str, i: int) -> None:
+        self.name = n
+        self.ID = i
+
+class Item():
+    def __init__(self, d:str, p: float) -> None:
+        self.description = d
+        self.price = p
+
+    def get_description(self):
+        return self.description
+
+    def get_price(self):
+        return self.price
+
+class Order():
+    def __init__(self, cust: Customer) -> None:
+        self.customer = cust
+        self.items = []
+        self.shipment = 0.
+        self.total = 0.
+
+    def add_item(self, item):
+        self.items.append(item)
+        self.total += item.price
+    
+    def set_shipment(self, ship: float):
+        self.shipment = ship
+    
+    def get_order(self):
+        # Function to return readable format
+        pass
